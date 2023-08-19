755a3131ab366411788d0e590b8d4f7065dfde3e
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-08-19 09:41:37 -0500

    feat(design): add confirm password validation

--- src/components/register-form/index.tsx
+++ src/components/register-form/index.tsx
@@ -4,9 +4,9 @@ import { Input } from "../ui/input";
 import { useForm } from "react-hook-form";
 import PasswordValidator from "password-validator";
 import { cn } from "@/lib/utils";
-import { useEffect, useState } from "react";
 import PasswordPolicyLabel from "../password-policy";
 import { Label } from "../ui/label";
+import TypographyMuted from "../typography/muted";
 
 const passwordPolicy = new PasswordValidator();
 passwordPolicy.is().min(8);
@@ -16,42 +16,51 @@ passwordPolicy.is().symbols();
 passwordPolicy.is().digits(1);
 
 export default function RegisterForm() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { defaultValues },
-  } = useForm<ISignUpUser>({
+  const { register, handleSubmit, watch } = useForm<ISignUpUser>({
     defaultValues: { email: "", name: "", password: "", repeatPassword: "" },
   });
   const passwordPolicyReviewer = passwordPolicy.validate(watch("password"), {
     list: true,
   }) as any[];
 
-  console.log(passwordPolicyReviewer);
+  const passwordMissMatch =
+    watch("password") !== watch("repeatPassword") && !!watch("repeatPassword");
+
   const onSubmit = (data: ISignUpUser) => {};
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
       className="w-[600px] mx-auto flex flex-col"
     >
-
-
       <div className="flex flex-col gap-y-4 mb-2">
-
-      <Label htmlFor="name">Name</Label>
-      <Input {...register("name")} placeholder="Full name" />
-      <Label htmlFor="email">Email</Label>
-      <Input {...register("email")} type="email" placeholder="Email" />
-      <Label htmlFor="password">Password</Label>
-      <Input {...register("password")} type="password" placeholder="Password" />
-      <Label htmlFor="password">Confirm password</Label>
-      <Input
-        {...register("repeatPassword")}
-        type="password"
-        placeholder="Repeat password"
+        <Label htmlFor="name">Name</Label>
+        <Input {...register("name")} placeholder="Full name" />
+        <Label htmlFor="email">Email</Label>
+        <Input {...register("email")} type="email" placeholder="Email" />
+        <Label htmlFor="password">Password</Label>
+        <Input
+          {...register("password")}
+          type="password"
+          placeholder="Password"
         />
-        </div>
+        <Label
+          className={cn({ "text-red-500": passwordMissMatch })}
+          htmlFor="repeatPassword"
+        >
+          Confirm password
+        </Label>
+        <Input
+          {...register("repeatPassword")}
+          type="password"
+          placeholder="Repeat password"
+          className={cn({ "border-red-500": passwordMissMatch })}
+        />
+        {passwordMissMatch && (
+          <TypographyMuted className="mt-[-12px] text-red-500">
+            Password do not match
+          </TypographyMuted>
+        )}
+      </div>
       <PasswordPolicyLabel
         isCorrect={!passwordPolicyReviewer.includes("digits")}
       >
--- src/components/typography/muted.tsx
+++ src/components/typography/muted.tsx
@@ -2,8 +2,8 @@ import { cn } from "@/lib/utils";
 
 interface Props {
   children: React.ReactNode;
-  className:string;
+  className?:string;
 }
-export default function TypographyMuted({ children,className }: Props) {
+export default function TypographyMuted({ children,className = '' }: Props) {
   return <p className={cn("text-sm text-muted-foreground",className)}>{children}</p>;
 }
