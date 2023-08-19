f08d474e151e6fd98adf192e02dc217252360f59
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-08-19 11:42:03 -0500

    feat: add sign-up feature

--- src/components/register-form/index.tsx
+++ src/components/register-form/index.tsx
@@ -10,6 +10,10 @@ import { Button } from "../ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signUpPasswordPolicy } from "@/src/utils/SignUpPasswordPolicy";
 import { SignUpSchema } from "@/src/schema/SignUpSchema";
+import { useMutation } from "@tanstack/react-query";
+import signUpUserAmplify from "@/src/services/user/mutations/signUpUserAmplify";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import { Loader2 } from "lucide-react";
 
 export default function RegisterForm() {
   const {
@@ -21,17 +25,25 @@ export default function RegisterForm() {
     defaultValues: { email: "", name: "", password: "", repeatPassword: "" },
     resolver: zodResolver(SignUpSchema),
   });
+
+  const { mutate, error, isError, isLoading, isSuccess } = useMutation(
+    ["signup-user"],
+    async (payload: ISignUpUser) => await signUpUserAmplify(payload)
+  );
+
   const passwordPolicyReviewer = signUpPasswordPolicy.validate(
     watch("password"),
-    {
-      list: true,
-    }
+    { list: true }
   ) as any[];
 
   const passwordMissMatch =
     watch("password") !== watch("repeatPassword") && !!watch("repeatPassword");
 
-  const onSubmit = (data: ISignUpUser) => {};
+  const onSubmit = (data: ISignUpUser) => {
+    mutate(data, {
+      onError: (error) => error,
+    });
+  };
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
@@ -89,17 +101,37 @@ export default function RegisterForm() {
       <PasswordPolicyLabel isCorrect={!passwordPolicyReviewer.includes("min")}>
         At least 8 char
       </PasswordPolicyLabel>
+      {isError && (
+        <Alert className="mt-4" variant={"destructive"}>
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{(error as any).message ?? ""}</AlertDescription>
+        </Alert>
+      )}
+      {isSuccess && (
+        <Alert className="mt-4" variant={"success"}>
+          <AlertTitle>Success</AlertTitle>
+          <AlertDescription>Account was created successfully!</AlertDescription>
+        </Alert>
+      )}
       <Button
         disabled={
           !isDirty ||
           !isValid ||
           !signUpPasswordPolicy.validate(watch("password")) ||
-          passwordMissMatch
+          passwordMissMatch ||
+          isLoading
         }
         className="mt-4"
         type="submit"
       >
-        Submit
+        {isLoading ? (
+          <>
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            Please wait
+          </>
+        ) : (
+          <>Sign Up</>
+        )}
       </Button>
     </form>
   );
