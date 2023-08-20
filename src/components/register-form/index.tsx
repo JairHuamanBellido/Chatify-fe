c9d5eebcb0f25d811b903ac9dd0f963bea6c7e84
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-08-19 22:18:51 -0500

    feat: add google sign up

--- src/components/register-form/index.tsx
+++ src/components/register-form/index.tsx
@@ -14,6 +14,8 @@ import { useMutation } from "@tanstack/react-query";
 import signUpUserAmplify from "@/src/services/user/mutations/signUpUserAmplify";
 import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
 import { Loader2 } from "lucide-react";
+import { Auth } from "aws-amplify";
+import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth";
 
 export default function RegisterForm() {
   const {
@@ -45,105 +47,131 @@ export default function RegisterForm() {
     });
   };
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="w-[600px] mx-auto flex flex-col"
-    >
-      <div className="flex flex-col gap-y-4 mb-2">
-        <Label htmlFor="name">Name</Label>
-        <Input
-          aria-label="name"
-          {...register("name")}
-          placeholder="Full name"
-        />
-        <Label htmlFor="email">Email</Label>
-        <Input
-          aria-label="email"
-          {...register("email")}
-          type="email"
-          placeholder="Email"
-        />
-        <Label htmlFor="password">Password</Label>
-        <Input
-          aria-label="password"
-          {...register("password")}
-          type="password"
-          placeholder="Password"
-        />
-        <Label
-          className={cn({ "text-red-500": passwordMissMatch })}
-          htmlFor="repeatPassword"
+    <div className="w-[600px] mx-auto">
+      <form onSubmit={handleSubmit(onSubmit)} className=" flex flex-col">
+        <div className="flex flex-col gap-y-4 mb-2">
+          <Label htmlFor="name">Name</Label>
+          <Input
+            aria-label="name"
+            {...register("name")}
+            placeholder="Full name"
+          />
+          <Label htmlFor="email">Email</Label>
+          <Input
+            aria-label="email"
+            {...register("email")}
+            type="email"
+            placeholder="Email"
+          />
+          <Label htmlFor="password">Password</Label>
+          <Input
+            aria-label="password"
+            {...register("password")}
+            type="password"
+            placeholder="Password"
+          />
+          <Label
+            className={cn({ "text-red-500": passwordMissMatch })}
+            htmlFor="repeatPassword"
+          >
+            Confirm password
+          </Label>
+          <Input
+            aria-label="repeat-password"
+            {...register("repeatPassword")}
+            type="password"
+            placeholder="Repeat password"
+            className={cn({ "border-red-500": passwordMissMatch })}
+          />
+          {passwordMissMatch && (
+            <TypographyMuted className="mt-[-12px] text-red-500">
+              Password do not match
+            </TypographyMuted>
+          )}
+        </div>
+        <PasswordPolicyLabel
+          isCorrect={!passwordPolicyReviewer.includes("digits")}
         >
-          Confirm password
-        </Label>
-        <Input
-          aria-label="repeat-password"
-          {...register("repeatPassword")}
-          type="password"
-          placeholder="Repeat password"
-          className={cn({ "border-red-500": passwordMissMatch })}
-        />
-        {passwordMissMatch && (
-          <TypographyMuted className="mt-[-12px] text-red-500">
-            Password do not match
-          </TypographyMuted>
+          At least 1 number
+        </PasswordPolicyLabel>
+        <PasswordPolicyLabel
+          isCorrect={!passwordPolicyReviewer.includes("symbols")}
+        >
+          At least 1 special character
+        </PasswordPolicyLabel>
+        <PasswordPolicyLabel
+          isCorrect={!passwordPolicyReviewer.includes("uppercase")}
+        >
+          At least 1 uppercase
+        </PasswordPolicyLabel>
+        <PasswordPolicyLabel
+          isCorrect={!passwordPolicyReviewer.includes("lowercase")}
+        >
+          At least 1 lowercase
+        </PasswordPolicyLabel>
+        <PasswordPolicyLabel
+          isCorrect={!passwordPolicyReviewer.includes("min")}
+        >
+          At least 8 char
+        </PasswordPolicyLabel>
+        {isError && (
+          <Alert className="mt-4" variant={"destructive"}>
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>{(error as any).message ?? ""}</AlertDescription>
+          </Alert>
         )}
+        {isSuccess && (
+          <Alert className="mt-4" variant={"success"}>
+            <AlertTitle>Success</AlertTitle>
+            <AlertDescription>
+              Account was created successfully!
+            </AlertDescription>
+          </Alert>
+        )}
+        <Button
+          disabled={
+            !isDirty ||
+            !isValid ||
+            !signUpPasswordPolicy.validate(watch("password")) ||
+            passwordMissMatch ||
+            isLoading
+          }
+          className="mt-4"
+          type="submit"
+        >
+          {isLoading ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </>
+          ) : (
+            <>Sign Up</>
+          )}
+        </Button>
+      </form>
+
+      <div className="relative mt-6">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
       </div>
-      <PasswordPolicyLabel
-        isCorrect={!passwordPolicyReviewer.includes("digits")}
-      >
-        At least 1 number
-      </PasswordPolicyLabel>
-      <PasswordPolicyLabel
-        isCorrect={!passwordPolicyReviewer.includes("symbols")}
-      >
-        At least 1 special character
-      </PasswordPolicyLabel>
-      <PasswordPolicyLabel
-        isCorrect={!passwordPolicyReviewer.includes("uppercase")}
-      >
-        At least 1 uppercase
-      </PasswordPolicyLabel>
-      <PasswordPolicyLabel
-        isCorrect={!passwordPolicyReviewer.includes("lowercase")}
-      >
-        At least 1 lowercase
-      </PasswordPolicyLabel>
-      <PasswordPolicyLabel isCorrect={!passwordPolicyReviewer.includes("min")}>
-        At least 8 char
-      </PasswordPolicyLabel>
-      {isError && (
-        <Alert className="mt-4" variant={"destructive"}>
-          <AlertTitle>Error</AlertTitle>
-          <AlertDescription>{(error as any).message ?? ""}</AlertDescription>
-        </Alert>
-      )}
-      {isSuccess && (
-        <Alert className="mt-4" variant={"success"}>
-          <AlertTitle>Success</AlertTitle>
-          <AlertDescription>Account was created successfully!</AlertDescription>
-        </Alert>
-      )}
       <Button
-        disabled={
-          !isDirty ||
-          !isValid ||
-          !signUpPasswordPolicy.validate(watch("password")) ||
-          passwordMissMatch ||
-          isLoading
-        }
-        className="mt-4"
-        type="submit"
+        onClick={async () => {
+          Auth.federatedSignIn({
+            provider: CognitoHostedUIIdentityProvider.Google,
+          });
+        }}
+        variant="outline"
+        type="button"
+        disabled={isLoading}
       >
-        {isLoading ? (
-          <>
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            Please wait
-          </>
-        ) : (
-          <>Sign Up</>
-        )}
+        Sign Up with Google
       </Button>
-    </form>
+    </div>
   );
 }
